a1bff216b4b71b3b19017d9481eb3afb8cf5eec2
Jared <jradtkey@gmail.com>
2018-02-23 10:57:40 -0800

    login

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-
-// 1
+import { BrowserRouter } from 'react-router-dom'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+
+
 // 2
-const httpLink = new HttpLink({ uri: 'http://localhost:3000' })
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
 // 3
 const client = new ApolloClient({
@@ -21,9 +22,11 @@ const client = new ApolloClient({
 
 // 4
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
-  , document.getElementById('root')
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
 )
 registerServiceWorker()
